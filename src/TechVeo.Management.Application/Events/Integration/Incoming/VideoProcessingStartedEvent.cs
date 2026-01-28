7596e6a1e172e7b2a8811003d1e61827291c5061
Valdeir Silva <svaldeirjose@gmail.com>
2026-01-27 23:05:38 -0300

    criando worker para gerenciar status dos videos

--- src/TechVeo.Management.Application/Events/Integration/Incoming/VideoProcessingStartedEvent.cs
+++ src/TechVeo.Management.Application/Events/Integration/Incoming/VideoProcessingStartedEvent.cs
@@ -2,7 +2,7 @@ using TechVeo.Shared.Application.Events;
 
 namespace TechVeo.Management.Application.Events.Integration.Incoming;
 
-public record VideoProcessingStarted(
+public record VideoProcessingStartedEvent(
     Guid VideoId,
     DateTime StartedAt
     ) : IIntegrationEvent;
